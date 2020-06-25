0f841c0288b40c4b9725bf6fe9378c784170efb0
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-06-25 18:15:28 +0800

    add progress bar and markdown textarea support

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,38 +1,90 @@
-$(document).ready( function() {
+/**
+ * main.js
+ */
+
+'use strict';
+
+$(document).ready( () => {
+    /* get array from url */
+    let getURL = () => {
+        let url = window.location.href;
+        let url_arr = url.split('?');
+        let get_dict = {};
+
+        url_arr.splice(0, 1);
+        url_arr.forEach( (element) => {
+                let values = element.split('=');
+                if (values.length > 1) {
+                    get_dict[values[0]] = values[1];
+                }
+            }
+        );
+
+        get_dict.length = url_arr.length;
+        return get_dict;
+    };
+
     /* get i18n language */
-    let lang = 'zh-TW'
+    let lang = 'zh-TW';
     let gets = getURL();
     if (gets.length > 0) {
         if (gets.lang == 'en') {
+            let en_name = ''; // write down your English name
             lang = 'en';
-            let eng_name = ''; // your English name
-            document.title = `${eng_name} - resume`
+            document.title = `${en_name} - Resume`;
         }
     }
 
-    /* render data from vuejs */
-    $.getJSON(`/data/lang/${lang}.json`, function(json) {
-        /* header */
-        let header = new Vue({
+    /* run progress bar */
+    let move = (pers) => {
+        var width = 1;
+        pers = pers > 100 ? 100 : pers;
+        let frame = () => {
+            if (width >= pers) {
+                clearInterval(id);
+            } else {
+                width++;
+                $("#myBar").width(width + "%");
+                $('#myNum').text(width + "%");
+            }
+        };
+        var id = setInterval(frame, 10);
+    };
+
+    /* get data from json file */
+    let data;
+    $.getJSON(`/data/lang/${lang}.json`, (json) => {
+        data = json;
+    }).done( () => {
+        /* render data from vuejs */
+        let header = new Vue({ /* header */
             el: '#vue-header',
             data: {
-                name: json.name,
-                title: json.title,
-                menu: json.header.menu
+                name: data.name,
+                title: data.title,
+                menu: data.header.menu
             },
-            mounted: function () {
+            mounted: () => {
+                /* animation scroll helper function */
+                let animationScrollTop = (top, speed, method) => {
+                    $('body, html').stop(true, true).animate({
+                        scrollTop: top
+                    }, speed, method);
+                };
+
                 /* nav menu animation */
-                $('.nav-wrapper ul li:not(:last-child) a').on('click', function () {
+                $('.nav-wrapper ul li:not(:last-child) a').on('click', () => {
                     event.preventDefault(); 
                     event.stopPropagation(); 
 
                     let current_herf = $(this).attr('href');
                     let href_top = $(current_herf).position().top - $('nav').height() - 7.5*2;
 
-                    ainmationScrollTop(href_top, 300, 'linear');
+                    animationScrollTop(href_top, 300, 'linear');
                 });
 
-                $('ul.sidenav li:not(:last-child) a').on('click', function () {
+                /* sidenav menu animation */
+                $('ul.sidenav li:not(:last-child) a').on('click', () => {
                     event.preventDefault(); 
                     event.stopPropagation(); 
 
@@ -41,61 +93,84 @@ $(document).ready( function() {
                     let current_herf = $(this).attr('href');
                     let href_top = $(current_herf).position().top - $('nav').height() - 7.5*2;
 
-                    ainmationScrollTop(href_top, 300, 'linear');
+                    animationScrollTop(href_top, 300, 'linear');
                 });
 
-                $('.brand-logo').on('click', function () {
+                /* nav logo animation */
+                $('.brand-logo').on('click', () => {
                     event.preventDefault(); 
                     event.stopPropagation(); 
 
-                    ainmationScrollTop(0, 300, 'linear');
+                    animationScrollTop(0, 300, 'linear');
                 });
 
+                /* set progress 0% -> 20% */
+                move(20);
             }
         });
 
-        /* footer */
-        let footer = new Vue({
+        let footer = new Vue({ /* footer */
             el: '#vue-footer',
             data: {
-                pdf: json.pdf
+                pdf: data.pdf
+            },
+            mounted: () => {
+                /* set progress 20% -> 40% */
+                move(40);
             }
         });
 
-        /* main */
-        let main = new Vue({
+        let main = new Vue({ /* main */
             el: '#vue-body',
             data: {
-                name: json.name,
-                title: json.title,
-                avator: json.avator,
-                about: json.about,
-                schools: json.edu,
-                exps: json.exp,
-                skills: json.skill,
-                projects: json.project,
-                infos: json.contact.data,
-                links: json.contact.link
+                name: data.name,
+                title: data.title,
+                avatar: data.avatar,
+                about: data.about,
+                schools: data.edu,
+                exps: data.exp,
+                skills: data.skill,
+                projects: data.project,
+                infos: data.contact.data,
+                links: data.contact.link
             },
             methods: {
-                offsetClass: function (index) {
+                offsetClass: (index) => {
                     return !(index % 2) ? 'offset-l2' : '';
                 } 
             },
-            mounted: function () {
+            mounted: () => {
                 /* materialize component initial */
                 $('.sidenav').sidenav();
                 $('textarea#message').characterCounter();
                 $('.parallax').parallax();
 
-                /* AOS initial */
-                AOS.init({
-                    easing: 'ease-in-out-sine',
-                    duration: 700
+                /* initial send mail textarea to markdown editor */
+                var simplemde = new SimpleMDE({ 
+                    element: $("#message")[0],
+                    status: false,
+                    toolbar: false, 
+                    toolbarTips: false,
                 });
 
+                /* set progress 40% -> 100% */
+                move(100);
+
+                /* wait for progress bar growing to 100% */
+                setTimeout( () => {
+                    /* close rendering mask */
+                    $('.before-render').hide();
+                    $('body').css({'overflow': "unset"});
+
+                    /* AOS initial */
+                    AOS.init({
+                        easing: 'ease-in-out-sine',
+                        duration: 700
+                    });
+                }, 2500);
+
                 /* post mail */
-                $('.send-mail').on('click', function (e) {
+                $('form button').on('click', (e) => {
                     event.preventDefault(); 
                     event.stopPropagation(); 
 
@@ -105,16 +180,18 @@ $(document).ready( function() {
                     let name = e.target.form[0].value;
                     let mail = e.target.form[1].value;
                     let subj = e.target.form[2].value;
-                    let mesg = e.target.form[3].value;
+                    let mesg = simplemde.value();
 
+                    /* check if any field is empty */
                     if (!name || !mail || !subj || !mesg) {
                         M.toast({html: error_str,  classes: 'failed'});
                     } else {
+                        /* or ajax post a mail from post-mail.php */
                         $.ajax({
                             type: "POST",
                             url: "/exec/post-mail.php",
                             data: { "name": name, "mail": mail, "subj": subj, "mesg": mesg},
-                            success: function (res) {
+                            success: (res) => {
                                 if (res == 'good') {
                                     M.toast({html: success_str, classes: 'successed'});
                                 } else {
@@ -123,36 +200,8 @@ $(document).ready( function() {
                             }
                         });
                     }
-                })
+                });
             }
         });
     });
 });
-
-function getURL() 
-{
-    let url = window.location.href;
-    let url_arr = url.split('?');
-    let get_dict = {};
-
-    url_arr.splice(0, 1);
-    url_arr.forEach(
-        function(element) {
-            let values = element.split('=');
-            if (values.length > 1) {
-                get_dict[values[0]] = values[1];
-            }
-        }
-    );
-
-    get_dict.length = url_arr.length;
-
-    return get_dict;
-}
-
-function ainmationScrollTop(top, speed, method)
-{
-    $('body, html').stop(true, true).animate({
-        scrollTop: top
-    }, speed, method);
-}
--- exec/post-mail.php
+++ exec/post-mail.php
@@ -1,26 +1,29 @@
 <?php
-$name = isset($_POST['name']) ? $_POST['name'] : '';
-$mail = isset($_POST['mail']) ? $_POST['mail'] : '';
-$subj = isset($_POST['subj']) ? $_POST['subj'] : '';
-$mesg = isset($_POST['mesg']) ? $_POST['mesg'] : '';
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $mail = isset($_POST['mail']) ? $_POST['mail'] : '';
+    $subj = isset($_POST['subj']) ? $_POST['subj'] : '';
+    $mesg = isset($_POST['mesg']) ? $_POST['mesg'] : '';
 
-if ($name && $mail && $subj && $mesg) {
-    $to      = ''; #your email to receive mail
-    $subject = $subj;
-    $message = $mesg;
-    $headers  = 'From: 履歷回信 <noreply@lusw.dev>' . "\r\n";
-    $headers .= 'Reply-To: '. $name . " <" . $mail . '>' . "\r\n";
-    $headers .= "Organization: LuSkywalker\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/plain; charset=utf-8\r\n";
-    $headers .= "X-Priority: 3\r\n";
-    $headers .= "Return-Path: noreply@lusw.dev\r\n";
-    $headers .= 'X-Mailer: PHP/' . phpversion();
+    if ($name && $mail && $subj && $mesg) {
+        include(dirname(__FILE__).'/Parsedown.php');
+        $Parsedown = new Parsedown();
 
-    $res = mail($to, $subject, $message, $headers);
+        $to      = ''; #your email to receive mail
+        $subject = $subj;
+        $message = $Parsedown->text($mesg);
+        $headers  = 'From: 履歷回信 <noreply@lusw.dev>' . "\r\n";
+        $headers .= 'Reply-To: '. $name . " <" . $mail . '>' . "\r\n";
+        $headers .= "Organization: LuSkywalker\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/plain; charset=utf-8\r\n";
+        $headers .= "X-Priority: 3\r\n";
+        $headers .= "Return-Path: noreply@lusw.dev\r\n";
+        $headers .= 'X-Mailer: PHP/' . phpversion();
 
-    echo $res? "good" : print_r(error_get_last());
-} else {
-    echo "請正確填寫表單!";
-}
+        $res = mail($to, $subject, $message, $headers);
+
+        echo $res? "good" : print_r(error_get_last());
+    } else {
+        echo "請正確填寫表單!";
+    }
 ?>
